f6c5ed205d2263f9463b3af0ea929a445fedd6bf
xrander <olamideadu@gmail.com>
2025-02-02 10:19:28 +0100

    start, pause/play and new game buttons added

--- main.js
+++ main.js
@@ -33,3 +33,47 @@ function guestadd3() {
     awayScoreValue += 3
     awayScore.textContent = awayScoreValue
 }
+
+let newGame = document.getElementById("start-new")
+
+function newgame() {
+    homeScoreValue = 0
+    homeScore.textContent = homeScoreValue
+    
+    awayScoreValue = 0
+    awayScore.textContent = awayScoreValue
+}
+
+let timer = document.getElementById("timer")
+let countdownInterval
+let isPaused = false
+let totalTime = 12 * 60 
+
+function startgame() {
+    clearInterval(countdownInterval)
+    totalTime = 12 * 60 // Reset to 12 minutes in seconds
+
+    countdownInterval = setInterval(updateTimer, 1000)
+}
+
+function updateTimer() {
+    let minutes = Math.floor(totalTime / 60)
+    let seconds = totalTime % 60
+
+    timer.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+
+    if (totalTime <= 0) {
+        clearInterval(countdownInterval)
+    } else {
+        totalTime -= 1
+    }
+}
+
+function pauseplay() {
+    if (isPaused) {
+        countdownInterval = setInterval(updateTimer, 1000)
+    } else {
+        clearInterval(countdownInterval)
+    }
+    isPaused = !isPaused
+}
